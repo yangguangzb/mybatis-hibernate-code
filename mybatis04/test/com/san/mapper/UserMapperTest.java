5e9c96712ffaa23f2f285ebdbe45c6e28fa36749
yangguangzb <2570886020@qq.com>
2018-03-26 14:52:58 +0800

    逆向工程

---
+++ mybatis04/test/com/san/mapper/UserMapperTest.java
@@ -0,0 +1,88 @@
+package com.san.mapper;
+
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import com.san.po.User;
+import com.san.po.UserExample;
+
+public class UserMapperTest implements UserMapper {
+	
+	
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@Test
+	public void testSelectByPrimaryKey() {
+		
+	}
+
+	@Override
+	public int countByExample(UserExample example) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteByExample(UserExample example) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteByPrimaryKey(Integer id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int insert(User record) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int insertSelective(User record) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<User> selectByExample(UserExample example) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User selectByPrimaryKey(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int updateByExampleSelective(User record, UserExample example) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateByExample(User record, UserExample example) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateByPrimaryKeySelective(User record) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateByPrimaryKey(User record) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
